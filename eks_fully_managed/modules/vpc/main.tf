4e98748eef3f83ca221f4e88a60f9b63d12823e5
nenisrudani7 <cavasakilala@gmail.com>
2026-01-13 12:25:39 +0530

    changes in github

--- eks_fully_managed/modules/vpc/main.tf
+++ eks_fully_managed/modules/vpc/main.tf
@@ -29,9 +29,12 @@ resource "aws_subnet" "public_subnet_az1" {
   availability_zone       = data.aws_availability_zones.available_zones.names[0]
   map_public_ip_on_launch = true
 
-  tags = {
-    mode = "precta"
-  }
+tags = {
+  mode = "precta"
+  "kubernetes.io/cluster/${var.project_name}_cluster" = "shared"
+  "kubernetes.io/role/elb" = "1"
+}
+
 }
 
 resource "aws_subnet" "public_subnet_az2" {

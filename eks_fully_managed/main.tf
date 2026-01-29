590683d9ad4a62d92ed93ab9d69ffc90b0d09dd5
nenisrudani7 <cavasakilala@gmail.com>
2026-01-29 17:56:02 +0530

    modified main.tf

--- eks_fully_managed/main.tf
+++ eks_fully_managed/main.tf
@@ -29,11 +29,6 @@ module "precta" {
   oidc_url             = "data.aws_eks_cluster.precta_dev.identity[0].oidc[0].issuer"
 }
 
- helm upgrade --install --namespace karpenter --create-namespace \
-  karpenter karpenter/karpenter \
-  --version 0.16.3 \
-  --set serviceAccount.annotations.eks\.amazonaws\.com/role-arn=arn:aws:iam::887675945169:role/KarpenterControllerRole-precta_cluster_eks \
-  --set clusterName=precta_cluster_eks \
-  --set clusterEndpoint=https://CD5C4D5C22FE4F17D80211C0B33D7582.gr7.us-east-1.eks.amazonaws.com \
-  --set aws.defaultInstanceProfile=KarpenterNodeInstanceProfile-precta_cluster_eks \
-  --wait # for the defaulting webhook to install before creating a Provisioner
+
+
+
